4cf54d261337f07d32e631cede453c63af12de52
cep <visualstudio.cep@hotmail.com>
2018-11-05 13:52:24 +0100

    Creacion Jsons

--- Olor a Libro/FormPrincipal.cs
+++ Olor a Libro/FormPrincipal.cs
@@ -1,8 +1,11 @@
-﻿using System;
+﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -12,6 +15,13 @@ namespace Olor_a_Libro
 {
     public partial class FormPrincipal : Form
     {
+        List<Libreria> librerias = new List<Libreria>();
+        List<Libro> libros = new List<Libro>();
+        List<Opinion> opiniones = new List<Opinion>();
+        List<Actividad> actividades = new List<Actividad>();
+        List<Usuario> usuarios = new List<Usuario>();
+        List<Visita> visitas = new List<Visita>();
+
         public FormPrincipal()
         {
             InitializeComponent();
@@ -35,6 +45,70 @@ namespace Olor_a_Libro
         private void FormPrincipal_Load(object sender, EventArgs e)
         {
             this.StartPosition = FormStartPosition.CenterScreen;
+            
+            //carregar/crear arxius json
+
+            string arxiulib = "librerias.json";
+            string arxiubook = "libros.json";
+            string arxiuuser = "usuarios.json";
+            string arxiuop = "opiniones.json";
+            string arxiuact = "actividades.json";
+            string arxiuviews = "visitas.json";
+
+            if (File.Exists(arxiulib))
+            {
+                JArray jArrayLibs = JArray.Parse(File.ReadAllText(arxiulib));
+                librerias = jArrayLibs.ToObject<List<Libreria>>();
+            }
+            else
+            {
+                librerias = new List<Libreria>();
+            }
+            if (File.Exists(arxiubook))
+            {
+                JArray jArrayBooks = JArray.Parse(File.ReadAllText(arxiubook));
+                libros = jArrayBooks.ToObject<List<Libro>>();
+            }
+            else
+            {
+                libros = new List<Libro>();
+            }
+            if (File.Exists(arxiuuser))
+            {
+                JArray jArrayusers = JArray.Parse(File.ReadAllText(arxiuuser));
+                librerias = jArrayusers.ToObject<List<Libreria>>();
+            }
+            else
+            {
+                usuarios = new List<Usuario>();
+            }
+            if (File.Exists(arxiuop))
+            {
+                JArray jArrayops = JArray.Parse(File.ReadAllText(arxiuop));
+                opiniones = jArrayops.ToObject<List<Opinion>>();
+            }
+            else
+            {
+                opiniones = new List<Opinion>();
+            }
+            if (File.Exists(arxiuact))
+            {
+                JArray jArrayacts = JArray.Parse(File.ReadAllText(arxiuact));
+                actividades = jArrayacts.ToObject<List<Actividad>>();
+            }
+            else
+            {
+                actividades = new List<Actividad>();
+            }
+            if (File.Exists(arxiuviews))
+            {
+                JArray jArrayviews = JArray.Parse(File.ReadAllText(arxiuviews));
+                visitas = jArrayviews.ToObject<List<Visita>>();
+            }
+            else
+            {
+                visitas = new List<Visita>();
+            }
         }
 
         private void libreriasToolStripMenuItem_Click(object sender, EventArgs e)
