c4a31707b96cf36c2545aafd776f47797e6f6022
cep <visualstudio.cep@hotmail.com>
2018-11-05 13:20:29 +0100

    Creacio de clases

---
+++ Olor a Libro/Libro.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Olor_a_Libro : IEquatable<Libro>
+{
+    class Libro
+    {
+        public int id { get; set; }
+        public string titulo { get; set; }
+        public string autor { get; set; }
+        public int anyo { get; set; }
+        public int precio { get; set; }
+        public List<string> genero { get; set; }
+
+        public bool Equals(Libro l)
+        {
+            bool igual = false;
+
+            if (l.titulo == this.titulo && l.autor == this.autor && l.anyo == this.anyo)
+            {
+                igual = true;
+            }
+            return igual;
+        }
+    }
+}
