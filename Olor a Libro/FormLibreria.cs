8eae9c22a0bcab2c95cf67363990926e33925d94
cep <visualstudio.cep@hotmail.com>
2018-11-13 11:18:54 +0100

    Añadir/editar librerias

--- Olor a Libro/FormLibreria.cs
+++ Olor a Libro/FormLibreria.cs
@@ -104,6 +104,14 @@ namespace Olor_a_Libro
         private void FormLibreria_Load(object sender, EventArgs e)
         {
             this.StartPosition = FormStartPosition.CenterScreen;
+            if(lib != null)
+            {
+                textBoxID.Text = lib.id.ToString();
+                textBoxNombreLib.Text = lib.nombre;
+                textBoxDirccionLib.Text = lib.direccion;
+                textBoxTelefonoLib.Text = lib.telefono.ToString();
+                textBoxCorreoLib.Text = lib.Correo;
+            }
         }
 
         public static Boolean repetido(Libreria lib)
@@ -120,29 +128,32 @@ namespace Olor_a_Libro
         private void buttonAceptar_Click(object sender, EventArgs e)
         {
             //Libreria lib = new Libreria();
-
+            int id = Utilitats.librerias.Count;
             String nombre = textBoxNombreLib.Text;
             String direccion = textBoxDirccionLib.Text;
             String telefono = textBoxTelefonoLib.Text;
             String correo = textBoxCorreoLib.Text;
-            String dirWeb = textBoxDirecWebLib.Text;
-            
+
+            //---------------------------------AÑADIR LIBRERIA------------------------------
             if (lib == null)
             {
-                if (nombre != null && direccion != "" && telefono != "" && correo != "" && dirWeb != "")
+
+                if (nombre != null && direccion != "" && telefono != "")
                 {
                     lib = new Libreria();
+                    lib.id = id;
                     lib.nombre = nombre;
                     lib.direccion = direccion;
-                    lib.Correo = correo;
                     lib.telefono = int.Parse(telefono);
+                    lib.Correo = correo;
 
                     Boolean encontrado = repetido(lib);
 
                     if (!encontrado)
                     {
                         Utilitats.librerias.Add(lib);
-                        MessageBox.Show("Libreria añadida satisfactoriamente", "Añadir Librería", MessageBoxButtons.OK, MessageBoxIcon.Hand);
+                        MessageBox.Show("Libreria añadida satisfactoriamente", "Añadir Librería", MessageBoxButtons.OK, MessageBoxIcon.Asterisk);
+                        this.Close();
                     }
                     else
                     {
@@ -154,66 +165,69 @@ namespace Olor_a_Libro
                     MessageBox.Show("No ha rellenado los campos", "Campos incompletos", MessageBoxButtons.OK, MessageBoxIcon.Error);
                     if (nombre == "")
                     {
-                        this.Focus();
+                        textBoxNombreLib.Focus();
                     }
                     else if (direccion == "")
                     {
-                        this.Focus();
+                        textBoxDirccionLib.Focus();
                     }
                     else if (telefono == "")
                     {
-                        this.Focus();
+                        textBoxTelefonoLib.Focus();
                     }
-                    else if (correo == "")
+                    /*else
                     {
                         this.Focus();
+                    }*/
+                }
+            }
+            //---------------------------------EDITAR LIBRERIA------------------------------
+            else
+            {
+                if (nombre != null && direccion != "" && telefono != "" && correo != "")
+                {
+                    lib.id = id;
+                    lib.nombre = nombre;
+                    lib.direccion = direccion;
+                    lib.telefono = int.Parse(telefono);
+                    lib.Correo = correo;
+
+                    Boolean encontrado = repetido(lib);
+
+                    if (!encontrado)
+                    {
+                        Utilitats.librerias.Add(lib);
+                        MessageBox.Show("Libreria añadida satisfactoriamente", "Añadir Librería", MessageBoxButtons.OK, MessageBoxIcon.Asterisk);
+                        this.Close();
                     }
                     else
                     {
-                        this.Focus();
+                        MessageBox.Show("Esta librería ya fue añadida.", "Librería repetida", MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
                     }
                 }
+                MessageBox.Show("No ha rellenado los campos", "Campos incompletos", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                if (nombre == "")
+                {
+                    textBoxNombreLib.Focus();
+                }
+                else if (direccion == "")
+                {
+                    textBoxDirccionLib.Focus();
+                }
+                else if (telefono == "")
+                {
+                    textBoxTelefonoLib.Focus();
+                }
+                /*else
+                {
+                    this.Focus();
+                }*/
             }
-            else
-            {
-
-            }
-
-            this.Close();
-            
-
         }
-        //AÑADIR / EDITAR LIBRERIA
-        /*
-        private void buttonAceptar_Click(object sender, EventArgs e)
+
+        private void buttonCancelar_Click(object sender, EventArgs e)
         {
-            if (lib1 == null)
-            {
-                Libreria l = new Libreria();
-                l.nombre = textBoxNombre.Text;
-                p.director = textBoxDirector.Text;
-                p.any = int.Parse(textBoxAnyo.Text);
-                p.tema = new List<string>();
-
-                foreach (string item in listBoxTemas.SelectedItems)
-                {
-                    p.tema.Add(item);
-                }
-
-                if (pelis.Contains(p))
-                {
-                    MessageBox.Show("La pelicula ja existeix", "ERROR", MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
-                }
-                else
-                {
-                    pelis.Add(p);
-                }
-            }
-            else
-            {
-
-            }
             this.Close();
-        }*/
+        }
     }
 }
